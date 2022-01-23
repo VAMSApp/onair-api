f9b82f30c3c61cc18364835a0ed08f5ff517ccba
Mike DeVita <mike@devita.co>
2022-01-23 16:41:35 -0700

    removed deprecated/unused fields

--- src/types/Company.ts
+++ src/types/Company.ts
@@ -1,21 +1,6 @@
-import { World } from './World';
-
-export interface CompanySkillPoint {
-  Id: string,
-  CompanyId: string,
-  Company: Record<string, unknown>,
-  Amount: number,
-  Type: number,
-  Tag: string,
-  Comment: string,
-  CreationDate: string
-
-}
-
 export interface Company {
   Id: string,
   WorldId: string,
-  World: World,
   Name: string,
   AirlineCode: string,
   LastConnection: string,
@@ -43,7 +28,6 @@ export interface Company {
   PayBonusFactor: number,
   EnableSimFailures: boolean,
   DisableSeatsConfigCheck: boolean,
-  CompanySkillPoints: CompanySkillPoint[]
   RealisticSimProcedures: boolean,
   TravelTokens: number,
   CurrentBadgeId: string,
